3570d581bb24cc7ed1582fc1fae29fec9d7fb541
mariocardenas1996 <mcardenaspadilla@gmail.com>
2025-09-01 19:24:31 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'territorios-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icono-192.png',
+  './icono-512.png',
+  './icono.ico',
+  './apple-icon.png',
+  // Agrega aquí todos los archivos usados por tu app:
+  './Territorio Entre Semana.png',
+  './Territorio Sabado.png',
+  './Territorio Domingo.png',
+  './territorio.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
